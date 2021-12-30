b7363a0cb7ba1607538a30cef1955c513b70d022
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2021-12-30 12:35:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ Client(
 ).start()
 
 
-print(f"[INFO]: BOT STARTED AS {BOT_NAME}!")
-print(f"[INFO]: ASSISTANT STARTED AS {ASSNAME}!")
+print(f"[INFO]: LUCY STARTED AS {BOT_NAME}!")
+print(f"[INFO]: LUCY ASSISTANT STARTED AS {ASSNAME}!")
 
 
 
 async def load_start():
     restart_data = await clean_restart_stage()
     if restart_data:
-        print("[INFO]: SENDING RESTART STATUS TO ZAID SERVER")
+        print("[INFO]: SENDING RESTART STATUS TO LUCY SERVER")
         try:
             await app.edit_message_text(
                 restart_data["chat_id"],
@@ -52,8 +52,8 @@ async def load_start():
             print("Error came while clearing db")
             pass     
     await app.send_message(LOG_GROUP_ID, "Music Bot Started")
-    await client.send_message(LOG_GROUP_ID, "Assistant Of Zaid Music Started")
-    print("[INFO]: STARTED THE ZAID BOT AND SENDING THE INFO TO ZAID SERVER")
+    await client.send_message(LOG_GROUP_ID, "Assistant Of LUCY Music Started")
+    print("[INFO]: STARTED THE LUCY BOT AND SENDING THE INFO TO LUCY SERVER")
     
    
 loop = asyncio.get_event_loop()
@@ -63,4 +63,4 @@ run()
 idle()
 loop.close()
 
-print("[LOG] CLOSING MUSIC BOT")
+print("[LOG] CLOSING LUCY MUSIC BOT")
